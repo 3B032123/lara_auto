7658e256130c30659ca27322a7f5ec97ec01ca0f
3B032123 <3b032123@gm.student.ncut.edu.tw>
2023-10-29 00:07:07 +0800

    Add composer monolog/monolog

--- public/index.php
+++ public/index.php
@@ -11,3 +11,15 @@ $vincent= new Hello\Someone('Vincent');
 
 $mary = new \Demo\Hello\Someone('Mary');
 $john = new Demo\Hello\Someone('John');
+
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('WISD');
+$log->pushHandler(new StreamHandler('../log/my.log', Level::Warning));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
